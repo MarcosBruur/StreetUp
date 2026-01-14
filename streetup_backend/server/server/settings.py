8802142c12483c1654f9c1b094a687f38e405334
M4rcosBrun0 <marcosbruur@gmail.com>
2026-01-14 10:53:35 -0300

    variables de entorno

--- streetup_backend/server/server/settings.py
+++ streetup_backend/server/server/settings.py
@@ -13,21 +13,25 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import mongoengine
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+import environ
 from datetime import timedelta
-
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pwi(wtslm=s5i&+s^bm#t-q5qljp4h)6me5toe667(6*o7@csk'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -35,7 +39,7 @@ DEBUG = False
 if DEBUG == True:
     ALLOWED_HOSTS = ['*']
 else:
-    ALLOWED_HOSTS = ['marcos.alexis.ar']
+    ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
@@ -94,17 +98,9 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 mongoengine.disconnect(alias="default")
 mongoengine.connect(
-    db='streetup',
-    host=os.environ.get("DB_URL")
+    db="streetup",
+    host=env("DB_URL")
 )
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
@@ -189,23 +185,14 @@ if DEBUG == True:
         "http://192.168.1.139",
     ]
 else:
-    CORS_ALLOWED_ORIGINS = [
-        "https://marcos.alexis.ar",
-    ]
+    CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS")
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = os.getenv('EMAIL_HOST')
-# EMAIL_PORT = os.getenv('EMAIL_PORT')
-# EMAIL_HOST_USER = os.getenv('EMAIL_USER')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
-# EMAIL_USE_TLS = True
-# DEFAULT_FROM_EMAIL = 'no-reply@tusitio.com'
-EMAIL_HOST = 'soslxzimc01.soslinux.com.ar'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'marcos.conegliano@cunix.net'
-DEFAULT_FROM_EMAIL = 'StreetUp <marcos.conegliano@cunix.net>'
-EMAIL_HOST_PASSWORD = 'Yt3md34+09'
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_USER")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 DATA_UPLOAD_MAX_MEMORY_SIZE = 50485760
